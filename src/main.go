6adc90801e0e3ff081c8141d7fd4fc9b4a6e4efc
森屋英治 <hmoriya@dell-w.archway.co.jp>
2018-01-17 13:28:42 +0900

    add template process

--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
@@ -12,6 +13,11 @@ func main() {
 	h := http.NewServeMux()
 	h.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Hello World")
+	})
+	h.HandleFunc("/render", func(w http.ResponseWriter, r *http.Request) {
+		t, _ := template.ParseFiles("template.html")
+		content := "I love architecture."
+		t.Execute(w, content)
 	})
 	err := http.ListenAndServe(":8888", h)
 	if err != nil {
