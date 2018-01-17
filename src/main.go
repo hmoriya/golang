58d6898c112e57ceab43274cec974a75c6cdd387
森屋英治 <hmoriya@macbookpro13hm.local>
2018-01-17 11:54:22 +0900

    add hello web app

--- src/main.go
+++ src/main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 )
 
 func main() {
 	log.Println("Hello World")
+
+	h := http.NewServeMux()
+	h.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello World")
+	})
+	err := http.ListenAndServe(":8888", h)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
