b036b81ec93feee464f92c0560a3b7d64b82a76c
森屋英治 <hmoriya@dell-w.archway.co.jp>
2018-01-18 17:14:05 +0900

    add static hander and index.html

--- src/main.go
+++ src/main.go
@@ -11,15 +11,19 @@ func main() {
 	log.Println("Hello World")
 
 	h := http.NewServeMux()
+	fs := http.FileServer(http.Dir("public"))
+	h.Handle("/", fs)
+
 	h.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Hello World")
 	})
 	h.HandleFunc("/render", func(w http.ResponseWriter, r *http.Request) {
-		t, _ := template.ParseFiles("template.html")
+		t, _ := template.ParseFiles("template/template.html")
 		content := "I love architecture."
 		t.Execute(w, content)
 	})
-	err := http.ListenAndServe(":8888", h)
+
+	err := http.ListenAndServe(":3000", h)
 	if err != nil {
 		log.Fatal(err)
 	}
